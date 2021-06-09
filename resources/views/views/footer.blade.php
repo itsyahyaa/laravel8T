7270b532c4720e473e13d87be0ea75bcc56ff892
Yahya Mohamed <yahyamohamed0001@gmail.com>
2021-06-09 13:07:51 +0300

    Add files via upload

---
+++ resources/views/views/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+    <h1>Footer View</h1>
+</footer>
+<style>
+    .footer{
+        background-color: aqua;
+        color: black;
+        padding: 20px 0;
+        text-align: center;
+        width: 100%;
+    }
+</style>
---
+++ resources/views/views/header.blade.php
@@ -0,0 +1,12 @@
+<header class="header">
+    <h1>Header View</h1>
+</header>
+<style>
+    .header{
+        background-color: aqua;
+        color: black;
+        padding: 20px 0;
+        text-align: center;
+        width: 100%;
+    }
+</style>
